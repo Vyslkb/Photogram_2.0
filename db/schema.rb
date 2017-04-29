7c913c699f3fc7154c0a65a609977909ac7eb9c4
zagpro1408 <zagorskybobuyc@gmail.com>
2017-04-29 12:40:29 +0300

    Add users_name field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429085857) do
+ActiveRecord::Schema.define(version: 20170429092340) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "caption"
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(version: 20170429085857) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true, using: :btree
   end
 
 end
