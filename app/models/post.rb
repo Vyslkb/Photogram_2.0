79ee36ac6614ffe919f3e7c8ccc6f4e7c804266b
zagpro1408 <zagorskybobuyc@gmail.com>
2017-05-03 08:20:20 +0300

    Add length validation for caption and add add time ago creation post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  validates :user_id, presence: true
-  validates :image, presence: true
+  validates :user_id, :image, presence: true
+  validates :caption, length: { minimum: 3, maximum: 300 }
 
   has_attached_file :image, styles: { medium: "640x" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
